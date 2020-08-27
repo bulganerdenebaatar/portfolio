e23e79fda98ea26c4270986b21d38d3c9ed837a1
Erdenebaatar <Erdenebaatar@ErdenebatarsAir.hsd1.wa.comcast.net>
2020-08-26 21:59:27 -0700

    Menu Button, javascript & rotate menu

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menuBranding');
+const navItem =document.querySelectorAll('.nav-items');
+
+//Set Initial State of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('close');
+        menuBranding.classList.add('close');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // Set Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // Set Menu State
+        showMenu = false;
+    }
+}
