96e47a2ddef199b4f99423c199d95d9ca547aaea
Bulgan Erdenebaatar <bulganebaatar@gmail.com>
2020-12-03 18:50:18 -0800

    complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,20 +56,23 @@ sr.reveal('.home__scroll', {delay: 200})
 sr.reveal('.home__img', {origin:'right', delay: 400})
 
 /* Scroll About */
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
+sr.reveal('.about__img', {origin:'right', delay: 500})
+sr.reveal('.about__subtitle', {origin:'left', delay: 300})
 sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
+sr.reveal('.about__text', {origin:'left', delay: 500})
 sr.reveal('.about__social-icon', {delay: 600, interval: 200})
 
 /* Scroll Skills */
-sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__subtitle', {origin:'left'})
 sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
 sr.reveal('.skills__img', {delay: 400})
 
 /* Scroll Portfolio */
-sr.reveal('.portfolio_img', {interval: 200})
+sr.reveal('.portfolio__img', {origin:'bottom',interval: 100})
 
 /* Scroll Contact */
-sr.reveal('.contact__subtitle',{})
+sr.reveal('.card',{interval: 100, delay: 300})
 sr.reveal('.contact__text',{delay: 400})
+
+/* Scroll Education */ 
+sr.reveal('.about__profession-span', {origin:'left', delay: 500, interval: 200})
